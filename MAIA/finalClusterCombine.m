4f856723d96f37d8804705e229785f95364cffeb
Mjdhsn49 <hussainmajid63@gmail.com>
2020-02-07 10:42:49 +0100

    proj

---
+++ MAIA/finalClusterCombine.m
@@ -0,0 +1,30 @@
+function LVseg = finalClusterCombine(combinedClusters,LVlocal)
+
+LVseg = zeros(size(combinedClusters));
+se = ones(3);
+
+for im = 1:size(combinedClusters,3)
+
+    % Erode the LVlocal some
+    currLVlocal = imerode(LVlocal(:,:,im),se);
+    currCluster = combinedClusters(:,:,im);
+    bg = mode(currCluster(:));
+    
+    u = unique(currLVlocal .* combinedClusters(:,:,im));
+    u(u==bg) = [];
+    
+    % Actually need to isolate the boundary now. Not the best way, but it
+    % works
+    tmp = ismember(combinedClusters(:,:,im),u);    
+    [B,L] = bwboundaries(tmp);
+    tmp = currLVlocal .* L;
+    bdr1 = mode(tmp(tmp~=0));
+    % Create a convex hull to smooth things
+    dt = delaunayTriangulation(B{bdr1}(:,1),B{bdr1}(:,2));
+    % Get the convex hull because the LV is always a circle in the
+    % short-axis orientation
+    ch = convexHull(dt);
+    LVseg(:,:,im) = roipoly(LVlocal(:,:,1), ...
+        round(dt.Points(ch,2)),round(dt.Points(ch,1)));
+        
+end
